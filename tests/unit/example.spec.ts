862726f56ba3ae51055dffd1e81ccb7543b9029b
husseinTalal2 <hossientalal2@gmail.com>
2021-11-17 00:37:58 +0300

    adds task component test

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,23 @@
 import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/Header.vue";
+import Header from "@/components/Header.vue";
+import Task from "@/components/Task.vue";
 
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
     const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
+    const wrapper = shallowMount(Header, {
       props: { msg },
     });
     expect(wrapper.text()).toMatch(msg);
+  });
+});
+
+describe("Task.vue", () => {
+  it("renders props.task when passed", () => {
+    const task = {task:"asx", finished:false};
+    const wrapper = shallowMount(Task, {
+      props: { task },
+    });
+    expect(wrapper.text()).toContain(task.task);
   });
 });
